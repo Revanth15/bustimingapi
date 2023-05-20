8d2db5b0137bc2b3b2f1c52c2974f095506ed017
Revanth <revanth1509@gmail.com>
2023-05-20 16:17:11 +0800

    testing IP tracking

--- website/dbs.py
+++ website/dbs.py
@@ -1,11 +1,8 @@
-import threading
-import time
-import schedule
+import socket
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
 import requests
 from datetime import datetime, timedelta, timezone
-# from . import db
 import json
 import os
 from dotenv import load_dotenv
@@ -15,7 +12,8 @@ dbs = Blueprint('dbs', __name__)
 
 @dbs.route('/healthcheckdbs', methods=['get'])
 def healthcheckdbs():  
-    user_ip = request.remote_addr
+    user_ip = socket.gethostbyname(socket.gethostname())
+    print(socket.gethostname())
     print(user_ip)
     response = "Healthy"
     return response
